b392fa37002df616a9cb7e3225b81734569635d0
Krzysztof Langner <klangner@gmail.com>
2017-04-24 15:30:26 +0200

    find wrong value

--- src/Main.purs
+++ src/Main.purs
@@ -2,14 +2,13 @@ module Main where
 
 import Prelude
 import Data.Array as A
-import Data.Maybe (fromMaybe)
 import Learn.Metrics.ConfusionMatrix as CM
 import Learn.Unsupervised.OutlierDetection as OC
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Data.Maybe (fromMaybe, fromJust)
-import Data.Tuple (Tuple(..))
+import Data.Tuple (Tuple(..), fst, snd)
 import Node.Encoding (Encoding(..))
 import Node.FS (FS)
 import Node.FS.Sync (readTextFile)
@@ -68,3 +67,8 @@ indexReport xs = do
   let idx1 = xs.index
   let idx2 = A.zipWith (\x1 x2 -> x2-x1) idx1 (fromMaybe [] (A.tail idx1))
   log $ "Index histogram " <> show (S.histogram idx2)
+
+  -- let idx3 = A.zip idx1 (fromMaybe [] (A.tail idx1))
+  -- let idx4 = A.filter (\tu -> (fst tu) > (snd tu)) idx3
+  -- log $ show idx4
+
